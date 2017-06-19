7885547328cceed8f7b25a2ab9825d221f9d1969
helyao <helyao@qq.com>
2017-06-19 19:46:08 +0800

    add baidu map to show

---
+++ show/createJSFile.py
@@ -0,0 +1,35 @@
+import os
+import sys
+import pymysql
+
+HOST = '192.168.1.100'
+PORT = 3306
+USER = 'helyao'
+PASSWD = 'welcome'
+DATABASE = 'mobike'
+TABLE = 'mobike_beijing_search'
+FILENAME = r'point.js'
+
+output = []
+
+sql = 'select x, y from {table}'.format(table=TABLE)
+connection = pymysql.connect(host=HOST, port=PORT, user=USER, passwd=PASSWD, db=DATABASE)
+
+try:
+    with connection.cursor() as cursor:
+        cursor.execute(sql)
+        results = cursor.fetchall()
+        for item in results:
+            output.append([item[0], item[1], 1])
+finally:
+    connection.close()
+
+print(len(output))
+
+content = {"data": output}
+
+with open(FILENAME, 'w') as f:
+    f.write('var data = {point};'.format(point=content))
+
+
+
